d5c09dbde46dd06027a8389e90c0215964bc1b1b
soultakis <psouleles@gmail.com>
2026-02-13 12:43:22 +0200

    added validation for the rules that apply to the firstname input field in the registration form

--- tests/Registration tests/test_firstname_field_registration_validation.py
+++ tests/Registration tests/test_firstname_field_registration_validation.py
@@ -14,6 +14,14 @@ def test_firstname_validate_firstname_minimum_length(page: Page, go_to_register_
         expect(register_page.success_message.all()[i]).to_have_text(registration_success_messages[i])
 
 
+def test_firstname_validate_firstname_maximum_length(page: Page, go_to_register_page):
+    register_page = go_to_register_page
+    register_page.prepare_firstname_validation()
+    register_page.enter_firstname_number_of_characters(max_length_input)
+    expect(register_page.successful_title).to_have_text(registration_success_title)
+    for i in range(len(register_page.success_message.all())):
+        expect(register_page.success_message.all()[i]).to_have_text(registration_success_messages[i])
+
 def test_empty_firstname_validation(page: Page, go_to_register_page):
     register_page = go_to_register_page
     register_page.prepare_firstname_validation()
@@ -21,15 +29,21 @@ def test_empty_firstname_validation(page: Page, go_to_register_page):
     registration_errors = RegistrationErrorLocators(page)
     expect(registration_errors.firstname_error_message).to_have_text(registration_firstname_error_message)
 
-
 def test_firstname_validate_firstname_length_less_than_minimum(page: Page, go_to_register_page):
     register_page = go_to_register_page
     register_page.prepare_firstname_validation()
-    register_page.enter_firstname_number_of_characters(2)
+    register_page.enter_firstname_number_of_characters(less_than_min_length_input)
     registration_errors = RegistrationErrorLocators(page)
     expect(registration_errors.firstname_error_message).to_be_visible()
     expect(registration_errors.firstname_error_message).to_have_text(registration_firstname_error_message)
 
+def test_firstname_validate_firstname_length_more_than_maximum(page: Page, go_to_register_page):
+    register_page = go_to_register_page
+    register_page.prepare_firstname_validation()
+    register_page.enter_firstname_number_of_characters(more_than_max_length_input)
+    registration_errors = RegistrationErrorLocators(page)
+    expect(registration_errors.firstname_error_message).to_be_visible()
+    expect(registration_errors.firstname_error_message).to_have_text(registration_firstname_error_message)
 
 
 
