f8ebc787a91027900b740f15d6f24413cc94d84c
soultakis <psouleles@gmail.com>
2026-02-19 13:23:57 +0200

    added validation for the rules that apply to the lastname input field in the registration form

--- pages/my_account.py
+++ pages/my_account.py
@@ -1,12 +1,15 @@
 from playwright.sync_api import Page, expect
 
-class UserHomePage:
+class MyAccount:
     def __init__(self, page: Page):
         self.page = page
         self.page.wait_for_selector("#content h2")
         self.page.locator("#content h2")
 
 
+
     def read_successful_logged_in_messages(self):
         locators_list = self.page.locator("#content h2").all()
         return locators_list
+
+    # def right_side_menu_list(self):
